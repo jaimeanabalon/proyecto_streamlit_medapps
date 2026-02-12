b31fba9043f72d0fc9ae4919a293fbb9fb308067
jaimeanabalon <jaimeanabalon@gmail.com>
2026-02-12 12:47:38 -0500

    Update prototipo_t_stager.py

--- the_etapificator_lung/prototipo_t_stager.py
+++ the_etapificator_lung/prototipo_t_stager.py
@@ -18,13 +18,13 @@ col1, col2, col3 = st.columns(3)
 with col1:
     st.subheader("Invasión T2")
     for i in t2_invasion:
-        st.checkbox(i, key=f"t2_{str(i)}")
+        st.checkbox(str(i), key=f"t2_{str(i)}")
 
 
 with col2:
     st.subheader("Invasión T3")
     for i in t3_invasion:
-        st.checkbox(i, key=f"t3_{str(i)}")
+        st.checkbox(str(i), key=f"t3_{str(i)}")
 
 with col3:
     st.subheader("Invasión T4")
