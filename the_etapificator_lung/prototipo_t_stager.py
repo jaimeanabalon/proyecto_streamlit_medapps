5798a850795780642a4c10715ce6f6affdfbc696
jaimeanabalon <jaimeanabalon@gmail.com>
2026-02-12 12:33:04 -0500

    Update prototipo_t_stager.py

--- the_etapificator_lung/prototipo_t_stager.py
+++ the_etapificator_lung/prototipo_t_stager.py
@@ -9,4 +9,4 @@ st.radio("Lateralidad del tumor", ["Izquierdo", "Derecho",], index=0, key="later
 
 diametro = st.number_input("Diámetro máximo del tumor (mm)", min_value=5, max_value=20, step=1, value="min", key="diametro")
 
-invasion = st.checkbox("Invasión de estructuras adyacentes", args = t2_invasion, key="invasion")
+invasion = st.selectbox("Invasión de estructuras adyacentes", args = t2_invasion, key="invasion")
