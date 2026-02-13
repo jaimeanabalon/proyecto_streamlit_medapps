1b247f3c3610c95e85d545166fe728d4415be7cf
jaimeanabalon <jaimeanabalon@gmail.com>
2026-02-13 11:07:12 -0500

    Update diccionarios_t.py

--- the_etapificator_lung/diccionarios_t.py
+++ the_etapificator_lung/diccionarios_t.py
@@ -5,14 +5,13 @@ t_invasion = {
     "t3": {
         "pleuropulmonar": ['pleura parietal','pared torácica'],
         "cardiovascular": ['pericardio','vena azigos'],
-        "nervioso": ['nervio frenico',,'raíces nerviosas torácicas','ganglio estrellado']
+        "nervioso": ['nervio frenico','raíces nerviosas torácicas','ganglio estrellado']
     },
     "t4": {
-        "pleuropulmonar":['traquea','carina']      
+        "pleuropulmonar":['traquea','carina'],
         "nervioso":['nv. laringeo recurrente','nv.vago','raices nerviosas cervicales','plexo braquial'],
         "cardiovascular":['corazón','vena cava inferior','vena cava superior','art o venas pulmonares intrapericardicas','arterias supraaorticas','venas braquiocefalicas','vasos subclavios'],
         "vertebral":['cuerpo vertebral','lamina vertebral','canal medular'],
         "visceral":['esofago','diafragma','timo']
     }
-
 }
